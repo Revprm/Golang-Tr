498268978b2a65ab253e8852e172e3abf2b117ef
Revprm <revy.pramana@gmail.com>
2023-11-19 09:02:34 +0700

    -

---
+++ 2-var/le/Imix/var.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	var msg = "Halo"
+	var a, b, c = 1, false, 3
+
+	fmt.Println(msg, a, b, c)
+}
---
+++ 2-var/le/Is/var.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// only in a function
+	message := "Hello"
+	a, b, c := 1, false, 3
+	d := 4
+	e := true
+	fmt.Println(message, a, b, c, d, e)
+}
